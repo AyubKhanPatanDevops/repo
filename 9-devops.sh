256b1bc6485d27cf4e4564942e90f88447686ce6
AyubDevopss <ayubkhan.pathanns@gmail.com>
2025-06-20 16:24:31 +0530

    loveluy

---
+++ 9-devops.sh
@@ -0,0 +1,26 @@
+#/bin/sh/!
+
+
+hello creating the steps 
+
+
+creat a one folder
+
+vuskea badh vus folder ka path ku git bash mea open karleana badh mea 
+
+badh clone karlena 
+ badh vus { vus folder:ku ;jako vs code mea open karna }
+
+ {{vus mea badh se files create kar ko rakhna }}
+
+ {{bad sea vus mea eak code marko 
+ 
+ git ststus 
+ 
+ git ku add karleana }}
+
+ {git commit karleana with the command 
+
+ git push karleana 
+
+ sure mujea devops mea job marleatham bro iam not a looser 
